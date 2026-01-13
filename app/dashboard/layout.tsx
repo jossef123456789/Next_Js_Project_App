247ec4de9f8ed52ca64347b1361df7cade724dd4
Bahri Youssef <elbahriyoussef061@gmail.com>
2026-01-13 10:55:22 +0100

    Add Next.js dashboard project structure

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "@/app/ui/dashboard/sidenav"; 
+// import React from 'React'; 
+export default function Layout({children} : { children: React.ReactNode }) {
+    return (
+        <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+            <SideNav />
+
+            <div className="grow p-6 md:overflow-y-auto md:p-12">
+                {children}
+            </div>
+        </div>
+    )
+}
